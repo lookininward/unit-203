d52245ae27968f3feeebdd0295930b35a2eb6bb7
lookininward <>
2022-06-23 19:22:37 -0400

    Can query DB for imported data; can filter by franchisee, location, and date; display query results in table; calculate and sum totals

---
+++ README.md
@@ -0,0 +1,5 @@
+# Unit 203
+
+Install Packages with `npm install`
+
+Start the app with `npm run start`
--- app.js
+++ app.js
@@ -1,11 +1,74 @@
 const express = require("express");
+const dayjs = require("dayjs");
+const path = require("path");
+const NodeJsonDB = require("node-json-db");
+const JsonDBConfig = require("node-json-db/dist/lib/JsonDBConfig");
+
 const app = express();
 const port = 3000;
 
-const path = require("path");
+const db = new NodeJsonDB.JsonDB(
+  new JsonDBConfig.Config("unitDB", true, false, "/")
+);
+
+db.push("/franchisees/", [
+  ...require("./data/franchisees.json").map((d) => ({
+    id: d._id,
+    firstName: d.first_name,
+    lastName: d.last_name,
+    locationIds: d.location_ids,
+  })),
+]);
+
+db.push("/locations/", [
+  ...require("./data/locations.json").map((d) => ({
+    id: d._id,
+    address: d.address,
+  })),
+]);
+
+db.push("/sales/", [
+  ...require("./data/sales.json").map((d) => ({
+    id: d._id,
+    franchiseeId: d.franchisee_id,
+    locationId: d.location_id,
+    date: d.date,
+    subtotal: d.subtotal,
+    tax: d.tax,
+    total: d.total,
+  })),
+]);
 
 app.get("/", (_, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/franchisees", (_, res) => {
+  res.send(db.getData("/franchisees"));
+});
+
+app.get("/locations", (_, res) => {
+  res.send(db.getData("/locations"));
+});
+
+app.get("/sales", (req, res) => {
+  const { franchisee, location, date } = req.query;
+
+  let data = db.getData("/sales");
+
+  if (franchisee) {
+    data = data.filter((i) => i.franchiseeId === franchisee);
+  }
+
+  if (location) {
+    data = data.filter((i) => i.locationId === location);
+  }
+
+  if (date) {
+    data = data.filter((i) => dayjs(i.date).format("YYYY-MM-DD") === date);
+  }
+
+  res.send(data);
 });
 
 app.listen(port, () => {
